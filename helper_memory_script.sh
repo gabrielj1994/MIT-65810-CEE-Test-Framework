ac033b83fc99a797622dabc4fe632dffe935ea7d
gabrielj1994 <gabrielj1994@gmail.com>
2022-12-19 18:57:42 -0500

    squash

--- helper_memory_script.sh
+++ helper_memory_script.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-sudo bash ./copy_proc.sh $1
-sudo bash ./pid_memory_dump.sh $1
 mkdir ~/dump_test_${2}_${1}
 sudo bash ./compare_memories.sh /opt/GIT/MIT-65810-CEE-Test-Framework/dump_busy_loop/$2 /opt/GIT/MIT-65810-CEE-Test-Framework/dump_busy_loop/$1 ~/dump_test_${2}_${1}
 grep 'Total differences:' ~/dump_test_${2}_${1}/*
