03568774c5ba2f77d3c2f600e94f37b7a1f9e551
gabrielj1994 <gabrielj1994@gmail.com>
2022-12-19 15:03:07 -0500

    squash: path scrubbing

--- compare_memories.sh
+++ compare_memories.sh
@@ -2,22 +2,27 @@
 
 pid_dir1=$1
 pid_dir2=$2
-filepaths1=("test1" "test2")
-filepaths2=("test3" "test4")
+filepaths1=()
+filepaths2=()
 
 # for str1 in ${filepaths1[@]}; do
 #   echo $str1
 # done
 
-for i in ${!filepaths1[@]}; do
-  echo "element $i is ${filepaths1[$i]} ${filepaths2[$i]}"
-done
+# for i in ${!filepaths1[@]}; do
+#   echo "element $i is ${filepaths1[$i]} ${filepaths2[$i]}"
+# done
 
 # printf ${filepaths1} | while read path ; do
 # printf "\nTesting"
 # printf ${path}
 # done
-exit 0
+
+ls -lrta ${pid_dir1} | grep output\d* | while read line ; do
+echo ${line}
+file_path=$(echo ${line} | awk '{print $8}')
+echo ${file_path}
+done
 
 # today=`date +'%s'`
 # hdfs dfs -ls /file/Path/ | grep "^d" | while read line ; do
