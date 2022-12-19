ff0ea84553af8151d254e6575ca2435f33efe621
gabrielj1994 <gabrielj1994@gmail.com>
2022-12-19 13:26:17 -0500

    65810: Memory Dump

--- pid_memory_dump.sh
+++ pid_memory_dump.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-#grep r[-w]-p ./dump_busy_loop/11312/maps \
-#| sed -n 's/^\([0-9a-f]*\)-\([0-9a-f]*\) .*$/\1 \2/p' \
+#-ex "shell grep r[-w]-p /proc/$pid/maps | sed -n 's/^\([0-9a-f]*\)-\([0-9a-f]*\) .*$/\1 \2/p' | while read start stop; do \
+#     gdb --batch --pid $1 -ex \
+#         "dump memory /proj/final-project/state/$fn/proc-maps-c$core-p$pid-r$start-$stop.log 0x$start 0x$stop"; \
+# done"
+# 
 #
 # grep rw-p /proc/$1/maps \
 grep busy_loop /proc/$1/maps \
